d7b570f70ca703014df512053acd2963a640aaf2
johnonneweer <j.p.t.onneweer@student.vu.nl>
2020-04-13 19:15:44 +0200

    windows compatible

--- describe.py
+++ describe.py
@@ -2,14 +2,25 @@ import pandas as pd
 import glob
 import sys
 
-path = "/Users/john/AI/Thesis/Data/AHN3"
-path_pijp = "/pijp/"
-path_utrecht = "/utrecht/"
-path_almere = "/almere/"
+win = 1
 
-files = [f for f in glob.glob(path + path_almere + "**/*.txt", recursive=True)]
+if win == 1:
+    path = r"C:\Users\Sustainables\Documents\Thesis\Data\AHN3"
+    path_pijp = r"\pijp\\"
+    path_utrecht = r"\utrecht\\"
+    path_almere = r"\almere\\"
+    path_hoogkarspel = r"\hoogkarspel\\"
+    path_test = r"\test\\"
+    
+    files = [f for f in glob.glob(path + path_test + "**/*.txt", recursive=True)]
 
-print(files)
+else:
+    path = "/Users/john/AI/Thesis/Data/AHN3"
+    path_pijp = "/pijp/"
+    path_utrecht = "/utrecht/"
+    path_almere = "/almere/"
+
+    files = [f for f in glob.glob(path + path_almere + "**/*.txt", recursive=True)]
 
 # sys.exit()
 
