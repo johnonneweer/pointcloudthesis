7d80603bfdea8fac0650e8642b3085152bf7c7de
johnonneweer <j.p.t.onneweer@student.vu.nl>
2020-04-17 15:01:37 +0200

    Update

--- describe.py
+++ describe.py
@@ -1,21 +1,52 @@
 import pandas as pd
+import numpy as np
 import glob
 import sys
 
-path = "/Users/john/AI/Thesis/Data/AHN3"
+path = "/Users/john/AI/Thesis/pointcloudthesis/pointcloudthesis/ahn3"
 path_pijp = "/pijp/"
 path_utrecht = "/utrecht/"
 path_almere = "/almere/"
+path_hoogkarspel = "/hoogkarspel/"
+path_gein = "/gein/"
 
-files = [f for f in glob.glob(path + path_almere + "**/*.txt", recursive=True)]
-
-print(files)
+path_location = path_utrecht
+files = [f for f in glob.glob(path + path_location + "**/*.txt", recursive=True)]
 
 # sys.exit()
 
+sc_1 = 0
+sc_2 = 0
+sc_6 = 0
+sc_9 = 0
+sc_26 = 0
+
 for f in files:
-    pc = pd.read_csv(f, names=['x', 'y', 'z', 'R', 'G', 'B', 'c', 'i', 'a'])
-    print(pc.c.unique())
+    # pc = pd.read_csv(f, names=['x', 'y', 'z', 'R', 'G', 'B', 'c', 'i', 'a'])
+    sc = np.loadtxt(f, delimiter=',', usecols=(6)).astype(np.int64)
+    count = np.count_nonzero(sc == 1)
+    sc_1 += count
+    count = np.count_nonzero(sc == 2)
+    sc_2 += count
+    count = np.count_nonzero(sc == 6)
+    sc_6 += count
+    count = np.count_nonzero(sc == 9)
+    sc_9 += count
+    count = np.count_nonzero(sc == 26)
+    sc_26 += count
+
+sc_t = sc_1 + sc_2 + sc_6 + sc_9 + sc_26
+
+print(" ")
+print("Details of fileset: " + path_location)
+print("-------------")
+print("Amount of Point Cloud Files are: " + str(len(files)))
+print("Amount with Class 1 --Vegetation/Other--: "+ str(sc_1))
+print("Amount with Class 2 --Ground--: "+ str(sc_2))
+print("Amount with Class 6 --Building--: "+ str(sc_6))
+print("Amount with Class 9 --Water--: "+ str(sc_9))
+print("Amount with Class 26 --Bridges/Art/Highway--: "+ str(sc_26))
+print("Total Points in dataset: " + str(sc_t))
 
 
 # import matplotlib.pyplot as plt
